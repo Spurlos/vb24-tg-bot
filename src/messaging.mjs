126ed4273d6529ea32079a48d87b7fb530a2727d
Vladislav Shumkin <spurlos2000@gmail.com>
2021-07-02 04:19:43 +0300

    Implement formatting for operation types

--- src/messaging.mjs
+++ src/messaging.mjs
@@ -1,28 +1,113 @@
-function convertTZ(date, timezoneString) {
-  return new Date(date).toLocaleString("en-GB", { timeZone: timezoneString });
-}
+import logger from "heroku-logger";
+import dedent from "dedent-js";
 
-function formatHistoryMessage(item) {
+function formatOperationMessage(item) {
+  let title;
+  let body;
   const {
     description,
-    merchantCategory,
-    operationTime,
-    transAmount,
+    operationType,
     totalAmount,
+    transAmount,
+    service,
+    fees,
+    location,
   } = item;
+
+  switch (operationType) {
+    case "transfer":
+      title = description;
+      body = `${totalAmount.value} <b>${totalAmount.currency}</b>`;
+      break;
+
+    case "payment":
+      {
+        let feeAmount = "";
+        if (fees.totalFee) {
+          feeAmount = ` (including ${fees.totalFee.value} <b>${fees.totalFee.currency}</b> fee)`;
+        }
+        title = service.name;
+        body = dedent`
+              ${totalAmount.value} <b>${totalAmount.currency}</b>${feeAmount}
+              <i>Customer ID</i>: ${service.shortFields.CUSTOM_IDT}
+            `;
+      }
+      break;
+
+    case "fee":
+      title = location.merchant;
+      body = dedent`
+            ${totalAmount.value} <b>${totalAmount.currency}</b>          
+            ${description}
+          `;
+      break;
+
+    case "interests":
+      title = description;
+      body = `${transAmount.value} <b>${transAmount.currency}</b>`;
+      break;
+
+    default:
+      logger.warn("Unknown operation type", { id: item.id, operationType });
+  }
+  body += `\n<i>Type</i>: ${operationType}`;
+
+  return { title, body };
+}
+
+function formatRegularMessage(item) {
   let accountCurrencyAmount = "";
+  const { merchantCategory, totalAmount, transAmount, location } = item;
 
   if (transAmount.currency !== totalAmount.currency) {
     accountCurrencyAmount = ` (${totalAmount.value} <b>${totalAmount.currency}</b>)`;
   }
 
-  return `<b>${description}</b>
+  const title = location.merchant;
+  let body = `${transAmount.value} <b>${transAmount.currency}</b>${accountCurrencyAmount}`;
 
-<i>Amount</i>: ${transAmount.value} <b>${
-    transAmount.currency
-  }</b>${accountCurrencyAmount}
-<i>Category</i>: #${merchantCategory}
-at ${convertTZ(operationTime, process.env.TIMEZEONE)}`;
+  if (location.country && location.city) {
+    body += `\n<i>Location</i>: ${location.country} ${location.city}`;
+  }
+
+  if (merchantCategory) {
+    body += `\n<i>Category</i>: #${merchantCategory}`;
+  }
+
+  return { title, body };
+}
+
+function convertTZ(date, timezoneString) {
+  return new Date(date).toLocaleString("en-GB", { timeZone: timezoneString });
+}
+
+function formatHistoryMessage(item) {
+  let message = "";
+
+  try {
+    const { body, title } = item.operationType
+      ? formatOperationMessage(item)
+      : formatRegularMessage(item);
+
+    if (title && body) {
+      message = dedent`
+        <b>${title}</b>
+        
+        ${body}
+        
+        at ${convertTZ(item.operationTime, process.env.TIMEZEONE)}
+      `;
+    }
+  } catch (exception) {
+    logger.warn(exception.message, { item });
+    message = dedent`
+      Error: Could not process transaction message
+  
+      <i>${item.id}</i>
+    `;
+  }
+
+  return message;
 }
 
 export default formatHistoryMessage;
